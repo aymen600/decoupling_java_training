56800149afa2b9d93494286e77a465eb57e7c31a
aymen600 <alahouel11@gmail.com>
2023-04-05 23:56:07 +0200

    correction part 6

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -3,7 +3,8 @@ package fr.lernejo.logger;
 public class LoggerFactory {
     public static Logger getLogger(String name) {
         //return new ContextualLogger(name, new ConsoleLogger());
-        return new ContextualLogger(name, new FileLogger("/home/aymen-lahouel/mylog.txt"));
+        //return new ContextualLogger(name, new FileLogger("/home/aymen-lahouel/mylog.txt"));
+        return new ContextualLogger(name, new CompositeLogger(new ConsoleLogger(), new FileLogger("/home/aymen-lahouel/mylog.txt")));
     }
 }
 
