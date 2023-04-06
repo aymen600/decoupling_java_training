3394cd60355c9bbc79a5ec4e746849e0130ba6e5
aymen600 <alahouel11@gmail.com>
2023-04-07 00:46:53 +0200

    end part 7

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,10 +1,18 @@
 package fr.lernejo.logger;
 
+import java.util.function.Predicate;
+
+
 public class LoggerFactory {
     public static Logger getLogger(String name) {
         //return new ContextualLogger(name, new ConsoleLogger());
         //return new ContextualLogger(name, new FileLogger("/home/aymen-lahouel/mylog.txt"));
-        return new ContextualLogger(name, new CompositeLogger(new ConsoleLogger(), new FileLogger("/home/aymen-lahouel/mylog.txt")));
+
+        //return new ContextualLogger(name, new CompositeLogger(new ConsoleLogger(), new FileLogger("/home/aymen-lahouel/mylog.txt")));
+        Predicate<String> condition = message -> message.contains("simulation");
+        Logger filteredLogger = new FilteredLogger(new FileLogger("/home/aymen-lahouel/mylog.txt"), condition);
+        return new ContextualLogger(name, new CompositeLogger(new ConsoleLogger(), filteredLogger));
     }
 }
+
 
